35f15eab939bdf56709fdf4a7c07dc1944737651
Ah.Nuril Huda <79719105+Nuril593@users.noreply.github.com>
2021-04-07 18:30:09 +0700

    Project First

---
+++ AI dengan image processing.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+#Start load an image
+imLoad = cv2.imread("ronaldo.jpg")
+#cv2.imshow("Window Name", imload)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#mpl.imshow(imLoad)
+#mpl.show()
+
+#cv2.imwrite("ronaldo",imload)
+
+
